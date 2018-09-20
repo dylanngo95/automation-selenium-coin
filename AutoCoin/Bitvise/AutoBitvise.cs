2b9c96e1744cf2db593c3c7ff431b278a8fbc4e6
jundat95 <it.tinhngo@gmail.com>
2018-09-21 01:11:40 +0700

    init automation

---
+++ AutoCoin/Bitvise/AutoBitvise.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium.Winium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCoin.Bitvise
+{
+    public class AutoBitvise
+    {
+        DesktopOptions options = new DesktopOptions();
+        WiniumDriver driver;
+
+        public AutoBitvise() {
+            options.ApplicationPath = @"C:\Program Files (x86)\Bitvise SSH Client\BvSsh.exe";
+            driver = new WiniumDriver(@"C:\Users\TinhNgo\coin\Winium.Desktop.Driver", options);
+        }
+
+        public void GetData() {
+
+        }
+
+        public void BindingWithName() {
+            driver.FindElementByName("Host").SendKeys("127.0.0.1:8099");
+        }
+    }
+}
---
+++ AutoCoin/Program.cs
@@ -0,0 +1,23 @@
+﻿using AutoCoin.Bitvise;
+using AutoCoin.GoogleSheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCoin
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //AutoBitvise autoBitvise = new AutoBitvise();
+            //autoBitvise.BindingWithName();
+
+            SheetApi sheetApi = new SheetApi();
+            sheetApi.Test();
+
+        }
+    }
+}
